b88fce895e8703c15a428036a770bf969ec49f88
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-03-15 15:21:39 -0600

    Casi terminado DE NUEVO

--- app/src/main/java/com/example/paneles/MainActivity.java
+++ app/src/main/java/com/example/paneles/MainActivity.java
@@ -2,14 +2,48 @@ package com.example.paneles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout mLinearLayout;
+
     private Button btn1, btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLinearLayout=findViewById(R.id.ll1);
+
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        Log.i("id", viewId+"");
+        switch (viewId){
+            case R.id.btn1:
+
+                break;
+            case R.id.btn2:
+
+                break;
+            case R.id.btn3:
+                
+                break;
+        }
     }
 }
